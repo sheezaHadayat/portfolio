bf9b5c40d32ef0d8e287948e3eaa5816631fa93f
sheezaHadayat <sheezahadayat@gmail.com>
2023-07-02 02:22:27 +0500

    final

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 // document.querySelector("")
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
+
+const linkIcons = document.querySelectorAll(".icons #link-icon");
+
+linkIcons.forEach((icon) => {
+    icon.addEventListener("click", function (event) {
+        event.preventDefault();
+        const link = this.getAttribute("href");
+        navigator.clipboard.writeText(link).then(function () {
+            alert("Link copied to clipboard!");
+        });
+    });
+});
+
+
+
 
